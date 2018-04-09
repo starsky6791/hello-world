e1616cdc70a57972a5bf5cb8324a8ec42847602b
starsky6791 <1334630295@qq.com>
2018-04-10 06:34:43 +0800

    增加了完整版SMO算法
    增加了完整版SMO算法

--- svmMLiA.py
+++ svmMLiA.py
@@ -86,6 +86,70 @@ def smoSimple(dataMatIn,classLabels,C,toler,maxIter):
         print('iteration number:%d'%(iterCount))   
     return alpha,b
 
+#用于储存所有数据，包括惩罚因子、误差、输入样本、输入样本的标签及需要最优化的参数α、b
+class optStruct:
+    def __init__(self,dataMat,labelMat,C,toler):
+        self.dataMat=dataMat
+        self.labelMat=labelMat
+        self.C=C
+        self.toler=toler
+        self.m=dataMat.shape()        
+        self.alphas=np.mat(np.zeros(self.m,1))
+        self.b=0
+        self.ELabel=np.mat(np.zeros(self.m,2))#第一列用于表示是否有效的标志，第二列给出E的值
+
+
+#用于计算E
+def calEK(oS,k):
+    E=float(np.multiply(oS.alphas*oS.labelMat).T*(oS.dataMat*oS.dataMat[k,:].T)+oS.b)\
+            -float(oS.labelMat[k])
+    return E
+
+#用于选择α2
+def selectJ(i,oS,EI):
+    maxdeltaE=0
+    maxJ=-1
+    EJ=0
+    for j in range(oS.m):
+        if j!=i:
+            EJ= calEK(oS,j)
+            if abs(EJ-EI)>maxdeltaE:
+                maxdeltaE=calEK(oS,j)-EI
+                maxJ=j
+    return maxJ,EJ
+
+
+def updataE(oS,k):
+    E=calEK(oS,k)
+    return E
+
+def innerL(i,oS):
+    #计算外层
+    EI=calEK(oS,i)
+    #不满足KKT条件是选择α
+    if((oS.alphas[i]>0)and(oS.labelMat[i]*EI>oS.toler))or\
+        ((oS.alphas[i]<oS.C)and(oS.labelMat[i]*EI<-oS.toler)):
+            j,EJ=selectJ(i,oS,EI)
+            alphaIOld=oS.alphas[i].copy()
+            alphaJOld=oS.alphas[j].copy()
+            #根据约束条件确定上下界
+            if (oS.labelMat[i] != oS.labelMat[j]):
+                L = max(0, oS.alphas[j] - oS.alphas[i])
+                H = min(oS.C, oS.C + oS.alphas[j] - oS.alphas[i])
+            else:
+                L = max(0, oS.alphas[j] + oS.alphas[i] - oS.C)
+                H = min(oS.C, oS.alphas[j] + oS.alphas[i])
+            if L==H: 
+                print ("L==H"); 
+                return 0
+            eta=float((oS.dataMat[i,:]-oS.dataMat[j,:])*(oS.dataMat[i,:]-\
+                          oS.dataMat[j,:]).T)
+            
+
+
+
+
+
 
 
 #用于调试代码
